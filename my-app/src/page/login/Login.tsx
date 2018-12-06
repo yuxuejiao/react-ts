d986a9b77d6b369c05f6f557155b523db612cee8
xuejiaoyu <67374152@qq.com>
2018-12-06 15:16:14 +0800

    update

--- my-app/src/page/login/Login.tsx
+++ my-app/src/page/login/Login.tsx
@@ -3,6 +3,7 @@ import {Button} from "antd";
 import {observer} from "mobx-react";
 import LoginStore from "./LoginStore";
 // import * as _ from "lodash";
+import * as styles from "./Login.pcss"
 @observer
 export default class Login extends React.Component<any> {
     public loginStore = new LoginStore();
@@ -14,7 +15,14 @@ export default class Login extends React.Component<any> {
 
     public render() {
         const {userName, setUserName} = this.loginStore;
-        return <><Button onClick={this.loginSuccess}>Login</Button><span>{userName}</span><a
-            onClick={() => setUserName("yyy")}>设置</a></>
+        return <>
+        <Button onClick={this.loginSuccess}>Login</Button>
+        <span
+            className={styles.test}>{userName}
+            <i>222</i>
+        </span>
+        {/*<div className={styles.rect}>Hello World</div>*/}
+        <a onClick={() => setUserName("yyy")}>设置</a>
+        </>
     }
 }
